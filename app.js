1cdd5421a33f91a02853b2b3f9d119f6a5dcad30
kamran <147907811+Code-with-kamran@users.noreply.github.com>
2024-07-13 16:29:22 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+// Activity 01  Variable Declaration
+
+// task 1
+var variable01 = 1;
+console.log(variable01);
+
+
+let str = "first challenge";
+console.log(str);
+
+
+// Activity 02 Constant Declaration
+
+const boool = true;
+console.log(boool);
+
+//  Activity 03 Data types
+
+let num = 56;
+let str1 = "data type";
+let bol = true;
+let arr = [1,3,4,5,5];
+let obj = {
+     name:"kamran",
+     age:22
+}
+
+console.log(typeof(num));
+console.log(typeof(str1));
+console.log(typeof(bol));
+console.log(typeof(arr));
+console.log(typeof(obj));
+
+
+// Activity 04 Reassigning Variables
+
+let var1 = 3;
+console.log(var1);
+var1 = 6;
+console.log(var1);
+
+
+// Activity 05 Understant const
+
+const constant1 = 5;
+
+// reassigning value to const give error
+// constant1 = 2;
+console.log(constant1);
